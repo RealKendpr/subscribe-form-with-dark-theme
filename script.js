783f145dd3e155815ce311304dde804ef4e916a4
dryk <88536325+thisguyisunknown@users.noreply.github.com>
2022-05-05 15:24:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function confirm() {
+  document.getElementById('confirm').classList.toggle("show");
+  setTimeout(() => {
+    document.getElementById("confirm").classList.remove('show');
+    const email = document.getElementById('email'); 
+    console.log(email.value);
+    email.value = '';
+    }, 1500);
+}
+function validateForm() {
+  var x = document.forms["Form"]["email"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  } else {confirm()} 
+}
+
+var form = document.getElementById("Form");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
